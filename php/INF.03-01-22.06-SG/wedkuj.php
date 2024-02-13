e1fe1375309eeede735c823f501eabc2659b9a4c
Witys112 <w.gasiewski@o2.pl>
2024-02-13 13:22:26 +0100

    poprawka

--- php/INF.03-01-22.06-SG/wedkuj.php
+++ php/INF.03-01-22.06-SG/wedkuj.php
@@ -2,7 +2,7 @@
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="styl_2.css">
+    <link rel="stylesheet" type="text/css" href="styl_1.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wędkowanie</title>
 </head>
